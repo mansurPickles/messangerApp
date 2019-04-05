5617f62b8cb072af6113007a91b2122eb6e8a5a7
Monsieur Pickles <brandon.luong@csu.fullerton.edu>
2019-04-05 16:48:36 -0700

    first

---
+++ Flash Chat/ChatViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  FaceLess Chat
+//
+//  Created by Brandon Luong on 05/05/2019.
+//  Copyright (c) All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class ChatViewController: UIViewController {
+    
+    // Declare instance variables here
+
+    
+    // We've pre-linked the IBOutlets
+    @IBOutlet var heightConstraint: NSLayoutConstraint!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var messageTextfield: UITextField!
+    @IBOutlet var messageTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //TODO: Set yourself as the delegate and datasource here:
+        
+        
+        
+        //TODO: Set yourself as the delegate of the text field here:
+
+        
+        
+        //TODO: Set the tapGesture here:
+        
+        
+
+        //TODO: Register your MessageCell.xib file here:
+
+        
+    }
+
+    ///////////////////////////////////////////
+    
+    //MARK: - TableView DataSource Methods
+    
+    
+    
+    //TODO: Declare cellForRowAtIndexPath here:
+    
+    
+    
+    //TODO: Declare numberOfRowsInSection here:
+    
+    
+    
+    //TODO: Declare tableViewTapped here:
+    
+    
+    
+    //TODO: Declare configureTableView here:
+    
+    
+    
+    ///////////////////////////////////////////
+    
+    //MARK:- TextField Delegate Methods
+    
+    
+
+    
+    //TODO: Declare textFieldDidBeginEditing here:
+    
+    
+    
+    
+    //TODO: Declare textFieldDidEndEditing here:
+    
+
+    
+    ///////////////////////////////////////////
+    
+    
+    //MARK: - Send & Recieve from Firebase
+    
+    
+    
+    
+    
+    @IBAction func sendPressed(_ sender: AnyObject) {
+        
+        
+        //TODO: Send the message to Firebase and save it in our database
+        
+        
+    }
+    
+    //TODO: Create the retrieveMessages method here:
+    
+    
+
+    
+    
+    
+    @IBAction func logOutPressed(_ sender: AnyObject) {
+        
+        //TODO: Log out the user and send them back to WelcomeViewController
+        do {
+        try Auth.auth().signOut()
+            navigationController?.popViewController(animated: true)
+        }
+        
+        catch {
+            print("signing out")
+        }
+    }
+    
+
+
+}
